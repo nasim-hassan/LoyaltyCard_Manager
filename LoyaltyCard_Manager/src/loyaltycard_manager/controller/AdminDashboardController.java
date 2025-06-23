6a3e9ddefd685c4a497f49fa94b4eba9fae4fa31
nasim-hassan <153202002+nasim-hassan@users.noreply.github.com>
2025-06-24 00:18:37 +0600

    Delete LoyaltyCard_Manager directory

--- LoyaltyCard_Manager/src/loyaltycard_manager/controller/AdminDashboardController.java
+++
@@ -1,5 +0,0 @@
-package loyaltycard_manager.controller;
-
-public class AdminDashboardController {
-    // TODO: Add logic for admin dashboard features (CRUD, etc.)
-}
--- LoyaltyCard_Manager/src/loyaltycard_manager/controller/CustomerDashboardController.java
+++
@@ -1,5 +0,0 @@
-package loyaltycard_manager.controller;
-
-public class CustomerDashboardController {
-    // TODO: Add logic for customer view (points, history)
-}
